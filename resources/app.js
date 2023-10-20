4a5c2d4dcd0eac85726e22ca186977b5cf6322a2
Schuh <141640357+Schuh1337@users.noreply.github.com>
2023-10-20 02:36:31 +0200

    Update app.js

--- resources/app.js
+++ resources/app.js
@@ -68,11 +68,11 @@ document.addEventListener('DOMContentLoaded', () => {
                 detect_on: 'window',
                 events: {
                     onhover: {
-                        enable: true,
+                        enable: false,
                         mode: 'repulse',
                     },
                     onclick: {
-                        enable: true,
+                        enable: false,
                         mode: 'push',
                     },
                     resize: true,
