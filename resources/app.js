d7d31c97cd89c1e14f11347549e593c4de1b9be5
Schuh <141640357+Schuh1337@users.noreply.github.com>
2023-10-25 08:53:54 +0200

    Add files via upload

--- resources/app.js
+++ resources/app.js
@@ -1,116 +1,72 @@
-document.addEventListener('DOMContentLoaded', () => {
-    particlesJS(
-        'particles-js',
-        {
-            particles: {
-                number: {
-                    value: 100,
-                    density: {
-                        enable: true,
-                        value_area: 1000,
-                    },
-                },
-                color: {
-                    value: '#fff',
-                },
-                shape: {
-                    type: 'circle',
-                    stroke: {
-                        width: 0,
-                        color: '#fff',
-                    },
-                    polygon: {
-                        nb_sides: 5,
-                    },
-                },
-                opacity: {
-                    value: 0,
-                    random: false,
-                    anim: {
-                        enable: false,
-                        speed: 1,
-                        opacity_min: 0.1,
-                        sync: false,
-                    },
-                },
-                size: {
-                    value: 5,
-                    random: true,
-                    anim: {
-                        enable: false,
-                        speed: 40,
-                        size_min: 0.1,
-                        sync: false,
-                    },
-                },
-                line_linked: {
-                    enable: true,
-                    distance: 150,
-                    color: '#fff',
-                    opacity: 0.4,
-                    width: 1,
-                },
-                move: {
-                    enable: true,
-                    speed: 3,
-                    direction: 'none',
-                    random: false,
-                    straight: false,
-                    out_mode: 'out',
-                    attract: {
-                        enable: false,
-                        rotateX: 600,
-                        rotateY: 1200,
-                    },
-                },
-            },
-            interactivity: {
-                detect_on: 'window',
-                events: {
-                    onhover: {
-                        enable: false,
-                        mode: 'repulse',
-                    },
-                    onclick: {
-                        enable: false,
-                        mode: 'push',
-                    },
-                    resize: true,
-                },
-                modes: {
-                    grab: {
-                        distance: 400,
-                        line_linked: {
-                            opacity: 1,
-                        },
-                    },
-                    bubble: {
-                        distance: 400,
-                        size: 40,
-                        duration: 2,
-                        opacity: 8,
-                        speed: 3,
-                    },
-                    repulse: {
-                        distance: 200,
-                    },
-                    push: {
-                        particles_nb: 0,
-                    },
-                    remove: {
-                        particles_nb: 2,
-                    },
-                },
-            },
-            retina_detect: true,
-            config_demo: {
-                hide_card: false,
-                background_color: '#b61924',
-                background_image: '',
-                background_position: '50% 50%',
-                background_repeat: 'no-repeat',
-                background_size: 'cover',
-            },
+    /* kinda useless but cool :P */
+    document.addEventListener('keydown', function (e) {if ((e.key === 'F12' || (e.ctrlKey && e.shiftKey && e.key === 'I')) || (e.ctrlKey && e.shiftKey && e.key === 'C')) {e.preventDefault();}});
+    document.addEventListener('keydown', function (e) {if (e.ctrlKey && e.key === 'u') {e.preventDefault();}});
+    document.addEventListener('keydown', function (e) {if (e.ctrlKey && e.key === 's') {e.preventDefault();}});
+    document.addEventListener('keydown', function (e) {if (e.ctrlKey && e.key === 'p') {e.preventDefault();}});
+    document.addEventListener('keydown', function (e) {if (e.ctrlKey && e.key === 'g') {e.preventDefault();}});
+    document.addEventListener('keydown', function (e) {if (e.ctrlKey && e.key === 'f') {e.preventDefault();}});
+    document.addEventListener('keydown', function (e) {if (e.ctrlKey && e.key === 'o') {e.preventDefault();}});
+    document.addEventListener('keydown', function (e) {if (e.ctrlKey && e.key === 'j') {e.preventDefault();}});
+    document.addEventListener('keydown', function (e) {if (e.ctrlKey && e.key === 'h') {e.preventDefault();}});
+    document.addEventListener('keydown', function (e) {if (e.ctrlKey && e.key === 'd') {e.preventDefault();}});
+    document.addEventListener('keydown', function (e) {if (e.ctrlKey && e.key === '+') {e.preventDefault();}});
+    document.addEventListener('keydown', function (e) {if (e.ctrlKey && e.key === '-') {e.preventDefault();}});
+    (function () {let SSWZ = function () {this.keyScrollHandler = function (e) {if (e.ctrlKey) {e.preventDefault(); return false;}}}; if (window === top) {let sswz = new SSWZ(); window.addEventListener('wheel', sswz.keyScrollHandler, { passive: false });}})();
+    document.addEventListener('contextmenu', function (e) {e.preventDefault();});
+    /*  */
+    var msg = "";
+    msg = "Schuh" + msg;
+    var position = 0;
+    function scrolltitle() {
+      document.title = msg.substring(position, msg.length) + msg.substring(0, position);
+      position++;
+      if (position > msg.length) position = 0;
+      window.setTimeout(scrolltitle, 400);
+    }
+    scrolltitle();
+
+    const popup = document.getElementById('popup');
+    const popupButton = document.getElementById('popupButton');
+    const popupCloseButton = document.getElementById('popupCloseButton');
+    let popupVisible = false;
+    popupButton.addEventListener('click', () => {
+        if (!popupVisible) {
+            popup.style.display = 'block';
+            setTimeout(() => {
+                popup.style.opacity = 1;
+            }, 15);
+            popupVisible = true;
+        } else {
+            closePopup();
         }
-    );
-});
+    });
+    popupCloseButton.addEventListener('click', () => {
+        closePopup();
+    });
+    function closePopup() {
+        popup.style.opacity = 0;
+        setTimeout(() => {
+            popup.style.display = 'none';
+        }, 400);
+        popupVisible = false;
+    }
+
+    const audioPlayer = document.getElementById('audioPlayer');
+    const playPauseButton = document.getElementById('playPauseButton');
+    let isPlaying = false;
+
+    audioPlayer.volume = 0.85;
+    audioPlayer.loop = true
+
+    playPauseButton.addEventListener('click', () => {
+      if (isPlaying) {
+        audioPlayer.pause();
+        playPauseButton.classList.remove('fa-pause');
+        playPauseButton.classList.add('fa-play');
+      } else {
+        audioPlayer.play();
+        playPauseButton.classList.remove('fa-play');
+        playPauseButton.classList.add('fa-pause');
+      }
+      isPlaying = !isPlaying;
+    });
