d33ff76f6776b1c2b5fcf1415114ddb4501d3ad0
Schuh <141640357+Schuh1337@users.noreply.github.com>
2023-12-07 14:09:52 +0100

    Update helpers.js

--- resources/slots/js/helpers.js
+++ resources/slots/js/helpers.js
@@ -1,7 +1,3 @@
-/**
- * @description Array shuffle
- * @returns {Array}
- */
 Array.prototype.shuffle = function () {
   for (let i = this.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
@@ -10,11 +6,6 @@ Array.prototype.shuffle = function () {
   return this;
 };
 
-/**
- * @description Generate random integer
- * @param max
- * @returns {number}
- */
 Number.prototype.randTo = function (max) {
   let min = this;
   min = Math.ceil(min);
@@ -23,31 +14,16 @@ Number.prototype.randTo = function (max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-/**
- * @description Create sequence generator to iterate
- * @param start
- * @param end
- * @returns {Generator<*, void, *>}
- */
 function* range(start, end) {
   for (let i = start; i <= end; i++) {
     yield i;
   }
 }
 
-/**
- * @description Array prototype Empty
- * @returns {boolean}
- */
 Array.prototype.empty = function () {
   return this.length === 0 ? !0 : !!0;
 };
 
-/**
- *
- * @param {number} num
- * @returns {{val: *, index: number}}
- */
 Array.prototype.closest = function (num) {
   let currVal = this[0],
     currIndex = 0;
@@ -64,11 +40,6 @@ Array.prototype.closest = function (num) {
   };
 };
 
-/**
- * @description DOM element selector
- * @param {HTMLElement|string} e
- * @returns {*}
- */
 const find = function (e) {
   if (typeof e === 'string') {
     return document.querySelector(e);
@@ -76,10 +47,6 @@ const find = function (e) {
   return e;
 };
 
-/**
- * @description Gets random element from the array
- * @returns {*}
- */
 Array.prototype.rnd = function () {
   return this[Math.floor(Math.random() * this.length)];
 };
